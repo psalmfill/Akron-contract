12ac924e9a7a0d40b7ef88f194bd23f6873f44c1
Samfield Hawb Bassey <samfield4sure@gmail.com>
2024-10-08 21:43:33 +0100

    minor fix

--- test/AKRONRevDistribution.js
+++ test/AKRONRevDistribution.js
@@ -3,22 +3,22 @@ const { ethers } = require("hardhat");
 
 describe("Akron Merged Contract with Multisig Governance", function () {
   let owner1, owner2, owner3, nonOwner, walletToPause, holder1, holder2;
-  let aKRONRevDistribution, akronMultiSign;
+  let aKRONRevDistribution, kronosMultiSig;
   const initialSupply = ethers.parseEther("1000");
 
   beforeEach(async function () {
     [owner1, owner2, owner3, nonOwner, walletToPause, holder1, holder2] =
       await ethers.getSigners();
 
-    const AkronMultiSign = await ethers.getContractFactory("KronosMultiSig");
+    const KronosMultiSig = await ethers.getContractFactory("KronosMultiSig");
 
-    akronMultiSign = await AkronMultiSign.deploy(
+    kronosMultiSig = await KronosMultiSig.deploy(
       [owner1.address, owner2.address, owner3.address],
       2
     );
     const AKRONRevDistribution = await ethers.getContractFactory("AKRONRevDistribution");
     aKRONRevDistribution = await AKRONRevDistribution.deploy(
-      await akronMultiSign.getAddress(),
+      await kronosMultiSig.getAddress(),
       initialSupply
     ); // 2 confirmations needed
   });
@@ -31,11 +31,11 @@ describe("Akron Merged Contract with Multisig Governance", function () {
       );
 
       // Propose and approve pause action by two owners
-      await akronMultiSign
+      await kronosMultiSig
         .connect(owner1)
         .proposeTransaction(await aKRONRevDistribution.getAddress(), pauseWalletData, 0);
-      await akronMultiSign.connect(owner2).approveTransaction(0);
-      await akronMultiSign.connect(owner3).approveTransaction(0);
+      await kronosMultiSig.connect(owner2).approveTransaction(0);
+      await kronosMultiSig.connect(owner3).approveTransaction(0);
 
       const isPaused = await aKRONRevDistribution.pausedWallets(walletToPause.address);
       expect(isPaused).to.equal(true);
@@ -69,11 +69,11 @@ describe("Akron Merged Contract with Multisig Governance", function () {
       );
 
       // Propose and approve pausing a wallet
-      await akronMultiSign
+      await kronosMultiSig
         .connect(owner1)
         .proposeTransaction(await aKRONRevDistribution.getAddress(), pauseWalletData, 0);
-      await akronMultiSign.connect(owner2).approveTransaction(0);
-      await akronMultiSign.connect(owner3).approveTransaction(0);
+      await kronosMultiSig.connect(owner2).approveTransaction(0);
+      await kronosMultiSig.connect(owner3).approveTransaction(0);
 
       let isPaused = await aKRONRevDistribution.pausedWallets(walletToPause.address);
       expect(isPaused).to.equal(true);
@@ -84,12 +84,12 @@ describe("Akron Merged Contract with Multisig Governance", function () {
       );
 
       // Propose and approve unpausing the wallet
-      await akronMultiSign
+      await kronosMultiSig
         .connect(owner1)
         .proposeTransaction(await aKRONRevDistribution.getAddress(), unpauseWalletData, 0);
       //   two approval
-      await akronMultiSign.connect(owner2).approveTransaction(1);
-      await akronMultiSign.connect(owner3).approveTransaction(1);
+      await kronosMultiSig.connect(owner2).approveTransaction(1);
+      await kronosMultiSig.connect(owner3).approveTransaction(1);
 
       isPaused = await aKRONRevDistribution.pausedWallets(walletToPause.address);
       expect(isPaused).to.equal(false);
@@ -103,15 +103,15 @@ describe("Akron Merged Contract with Multisig Governance", function () {
       );
 
       // Propose and approve updating the claim interval
-      await akronMultiSign
+      await kronosMultiSig
         .connect(owner1)
         .proposeTransaction(
           await aKRONRevDistribution.getAddress(),
           updateClaimIntervalData,
           0
         );
-      await akronMultiSign.connect(owner2).approveTransaction(0);
-      await akronMultiSign.connect(owner3).approveTransaction(0);
+      await kronosMultiSig.connect(owner2).approveTransaction(0);
+      await kronosMultiSig.connect(owner3).approveTransaction(0);
 
       const claimInterval = await aKRONRevDistribution.claimInterval();
       expect(claimInterval).to.equal(newInterval);
@@ -150,7 +150,7 @@ describe("Akron Merged Contract with Multisig Governance", function () {
     // });
     it("Should successfully blacklist a wallet", async function () {
       // Propose blacklisting addr1
-      await akronMultiSign
+      await kronosMultiSig
         .connect(owner1)
         .proposeTransaction(
           await aKRONRevDistribution.getAddress(),
@@ -161,8 +161,8 @@ describe("Akron Merged Contract with Multisig Governance", function () {
         );
 
       // Approve by 2 owners to reach required confirmations
-      await akronMultiSign.connect(owner1).approveTransaction(0); // owner confirms
-      await akronMultiSign.connect(owner2).approveTransaction(0); // addr2 confirms and executes
+      await kronosMultiSig.connect(owner1).approveTransaction(0); // owner confirms
+      await kronosMultiSig.connect(owner2).approveTransaction(0); // addr2 confirms and executes
 
       // Check if holder1 is blacklisted
       const isBlacklisted = await aKRONRevDistribution.blacklistedWallets(holder1.address);
@@ -171,7 +171,7 @@ describe("Akron Merged Contract with Multisig Governance", function () {
 
     it("Should successfully unblacklist a wallet", async function () {
       // First, blacklist holder1
-      await akronMultiSign
+      await kronosMultiSig
         .connect(owner1)
         .proposeTransaction(
           await aKRONRevDistribution.getAddress(),
@@ -180,15 +180,15 @@ describe("Akron Merged Contract with Multisig Governance", function () {
           ]),
           0
         );
-      await akronMultiSign.connect(owner1).approveTransaction(0);
-      await akronMultiSign.connect(owner2).approveTransaction(0);
+      await kronosMultiSig.connect(owner1).approveTransaction(0);
+      await kronosMultiSig.connect(owner2).approveTransaction(0);
 
       // Confirm holder1 is blacklisted
       let isBlacklisted = await aKRONRevDistribution.blacklistedWallets(holder1.address);
       expect(isBlacklisted).to.be.true;
 
       // Propose unblacklisting holder1
-      await akronMultiSign
+      await kronosMultiSig
         .connect(owner1)
         .proposeTransaction(
           await aKRONRevDistribution.getAddress(),
@@ -199,8 +199,8 @@ describe("Akron Merged Contract with Multisig Governance", function () {
         );
 
       // Approve the unblacklist action
-      await akronMultiSign.connect(owner1).approveTransaction(1);
-      await akronMultiSign.connect(owner2).approveTransaction(1);
+      await kronosMultiSig.connect(owner1).approveTransaction(1);
+      await kronosMultiSig.connect(owner2).approveTransaction(1);
 
       // Check if holder1 is unblacklisted
       isBlacklisted = await aKRONRevDistribution.blacklistedWallets(holder1.address);
@@ -221,7 +221,7 @@ describe("Akron Merged Contract with Multisig Governance", function () {
         holder2.address
       );
       // Blacklist addr1
-      await akronMultiSign
+      await kronosMultiSig
         .connect(owner1)
         .proposeTransaction(
           await aKRONRevDistribution.getAddress(),
@@ -230,8 +230,8 @@ describe("Akron Merged Contract with Multisig Governance", function () {
           ]),
           0
         );
-      await akronMultiSign.connect(owner1).approveTransaction(0);
-      await akronMultiSign.connect(owner2).approveTransaction(0);
+      await kronosMultiSig.connect(owner1).approveTransaction(0);
+      await kronosMultiSig.connect(owner2).approveTransaction(0);
 
       // Confirm addr1 is blacklisted
       const isBlacklisted = await aKRONRevDistribution.blacklistedWallets(holder1.address);
@@ -242,7 +242,7 @@ describe("Akron Merged Contract with Multisig Governance", function () {
       const balances = [ethers.parseEther("500"), ethers.parseEther("500")]; // Both hold 50% of the total supply
 
       // Propose revenue distribution action
-      await akronMultiSign
+      await kronosMultiSig
         .connect(owner1)
         .proposeTransaction(
           await aKRONRevDistribution.getAddress(),
@@ -254,8 +254,8 @@ describe("Akron Merged Contract with Multisig Governance", function () {
         );
 
       // Approve and execute the revenue distribution action
-      await akronMultiSign.connect(owner1).approveTransaction(1);
-      await akronMultiSign.connect(owner2).approveTransaction(1);
+      await kronosMultiSig.connect(owner1).approveTransaction(1);
+      await kronosMultiSig.connect(owner2).approveTransaction(1);
 
       const contractBalance = await ethers.provider.getBalance(
         await aKRONRevDistribution.getAddress()
